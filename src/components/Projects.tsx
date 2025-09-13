d25a7f46e0d3f58e349af0c473057eae4b154b0a
Dan <gooddiscord@gmail.com>
2025-09-12 20:08:24 -0700

    fix

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import type { Project } from '../types';
 import './Projects.css';
 
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import './Skills.css';
 
 interface SkillsProps {
